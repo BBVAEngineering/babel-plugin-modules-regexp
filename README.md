09730bbdee45317b1240e8a60f8a639eb6f75af0
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2017-09-25 09:18:57 +0200

    [feat] Apply regexp to module name and imports on AST traverse.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # babel-plugin-modules-regexp
-Bypass framework limitation to configure babel options like `getModuleId` and `resolveModuleSource`.
+
+## Motivation
+
+Bypass framework limitation to configure babel options like `getModuleId`.
+
+This plugin injects a regexp to replace default module names.
+
+## Usage
+
+```
+{
+  "plugins": [
+    ["babel-plugin-modules-regexp", {
+      regexp: /(foo)/,
+      substr: 'test/$1'
+    }]
+  ]
+}
+
+```
---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function() {
+  return {
+    pre(file) {
+      const opts = file.opts;
+      const getModuleId = opts.getModuleId;
+      const regexp = this.opts.regexp;
+      const substr = this.opts.substr;
+
+      opts.getModuleId = moduleName => {
+        moduleName = getModuleId ? getModuleId.apply(opts, moduleName) : moduleName;
+
+        return moduleName.replace(regexp, substr);
+      };
+    },
+    visitor: {
+      ImportDeclaration(path) {
+        const regexp = this.opts.regexp;
+        const substr = this.opts.substr;
+
+        path.node.source.value = path.node.source.value.replace(regexp, substr);
+      },
+    },
+  };
+};
+
+module.exports.baseDir = function() {
+  return __dirname;
+};
